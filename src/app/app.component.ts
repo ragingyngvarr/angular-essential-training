4cd1bd2e1cf6b04ab0ff00e5cb9b47a0be15b996
Ihar Krasiuk <ihar.krasiuk@refinitiv.com>
2021-06-25 10:06:47 +0300

    2 Components

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'mw-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+    firstMediaItem = {
+        id: 1,
+        name: 'Firebug',
+        medium: 'Series',
+        category: 'Science Fiction',
+        year: 2010,
+        watchedOn: 1294166565384,
+        isFavorite: false
+    };
+    onMediaItemDelete(mediaItem) {
+        console.log(mediaItem);
+    }
+}
