e89d1a9c3825a2b2e29398f736b7c9a45430f044
ragingyngvarr <yngvarr.bel@gmail.com>
2021-06-26 14:22:02 +0300

    4 Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item/media-item.component';
 import { MediaItemListComponent } from './media-item-list/media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
+import { MediaItemFormComponent } from './media-item-form/media-item-form.component';
 
 @NgModule({
     imports: [
         BrowserModule,
+        ReactiveFormsModule,
     ],
     declarations: [
         AppComponent,
@@ -16,6 +19,7 @@ import { CategoryListPipe } from './category-list.pipe';
         MediaItemListComponent,
         FavoriteDirective,
         CategoryListPipe,
+        MediaItemFormComponent,
     ],
     bootstrap: [
         AppComponent,
---
+++ src/app/media-item-form/media-item-form.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MediaItem } from '../types/media-item';
+
+@Component({
+  selector: 'mw-media-item-form',
+  templateUrl: './media-item-form.component.html',
+  styleUrls: ['./media-item-form.component.css']
+})
+export class MediaItemFormComponent implements OnInit {
+  form: FormGroup;
+  constructor() { }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      medium: new FormControl('Movies'),
+      name: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
+      category: new FormControl(''),
+      year: new FormControl('', this.yearValidator)
+    });
+  }
+
+  yearValidator(control: FormControl) {
+    if (control.value.trim().length === 0){
+      return null;
+    }
+    const year = parseInt(control.value, 10);
+    const minYear = 1900;
+    const maxYear = 2100;
+    if (year >= minYear && year <= maxYear) {
+      return null;
+    } else {
+      return { year: {
+        min: minYear,
+        max: maxYear,
+      } };
+    }
+  }
+
+  onSubmit(mediaItem: MediaItem) {
+    console.log(mediaItem);
+  }
+
+}
